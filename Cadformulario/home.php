0499a6be85f964dc7ab4653b4ad5974b95bbfabc
Rennan <91549549+RennanXD@users.noreply.github.com>
2023-02-20 15:54:01 -0300

    Adicionando uma home

---
+++ Cadformulario/home.php
@@ -0,0 +1,17 @@
+<hmtl>
+    <head>
+        <title>Banco fafafas</title>
+    </head>
+   <?php
+    echo "Bem vindo ao banco fafas";
+    echo "Escolha o que deseja fazer";
+?>
+    <form action="entrada.php">
+        <input type="subimt" value="Entrar">
+    </form>
+
+    <form action="index.php">
+        <input type="subimit" value="Cadastro">
+    </form>
+
+</hmtl>
